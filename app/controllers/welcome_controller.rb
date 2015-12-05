f45faaa5599f461f637e524d1ce3e2c6492cd234
Cang Ta <hoksilato176@gmail.com>
2015-12-05 15:31:05 +0700

    change contact route

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,4 @@
 class WelcomeController < ApplicationController
   def index
   end
-
-  def contact
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'contact' => 'contact#index'
 
-  get 'welcome/contact'
+  resources :menus
+
+  get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
