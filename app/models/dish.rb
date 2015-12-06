b0bd32ebf465eddda8b4c80bdf7392273f7a1af5
Cang Ta <hoksilato176@gmail.com>
2015-12-07 00:06:10 +0700

    add validations for dish

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,16 @@
 class Dish < ActiveRecord::Base
   belongs_to :menu
   paginates_per 9
+
+  validates :name, presence: true,
+                    length: { minimum: 5 },
+                    uniqueness: true
+  validates :price, presence: true
+  validates_numericality_of :price, :greater_than => 0
+
+  validates_associated :menu
+
+  validates_each :name do |record, attr, value|
+    record.errors.add(attr, 'must start with upper case') if value =~ /\A[[:lower:]]/
+  end
 end
